6d65b4abe65d71ac7f8d58cadbd872ceb8a9fd6f
Rebeka Koszticsák <rebebaba2@gmail.com>
2019-01-05 19:40:46 +0100

    slightly optimized face detection

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <android/log.h>
 #include "opencv2/opencv.hpp"
+#include "opencv2/video/tracking.hpp"
 //#include "opencv2/objdetect/objdetect.hpp"
 
 
@@ -16,20 +17,84 @@ Java_cvsp_whitechristmas_MainActivity_stringFromJNI(
 }
 
 
-
+std::vector<cv::Rect> faces;
+cv::String face_cascade_name = "/storage/emulated/0/Download/haarcascade_frontalface_default.xml";
+bool faceCascadedLoaded = false;
+cv::CascadeClassifier face_cascade;
+cv::Mat prevFrame, prevprevFrame;
+int frameCounter = 0;
 extern "C"
 JNIEXPORT void JNICALL
 Java_cvsp_whitechristmas_OpencvCalls_faceDetection(JNIEnv *env, jclass type, jlong addrRgba) {
 
+
+        cv::Mat &frame = *(cv::Mat *) addrRgba;
+
+        if (!faceCascadedLoaded) {
+            if (!face_cascade.load(face_cascade_name)) {
+                __android_log_print(ANDROID_LOG_INFO, "OpenCVCalls", "Error loading face cascade");
+                return;
+            } else {
+                faceCascadedLoaded = true;
+            }
+        }
+        cv::Mat frame_gray;
+
+        cvtColor(frame, frame_gray, CV_BGR2GRAY);
+        cv::equalizeHist(frame_gray, frame_gray);
+        cv::rotate(frame_gray, frame_gray, cv::ROTATE_90_CLOCKWISE);
+
+        if (frameCounter == 0) {
+            prevprevFrame = frame;
+        } else if (frameCounter == 1) {
+            prevFrame = frame;
+        }
+        //-- Detect faces
+        face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE,
+                                      cv::Size(30, 30));
+
+        for (size_t i = 0; i < faces.size(); i++) {
+            cv::Point center(faces[i].y + faces[i].height * 0.5,
+                             frame.rows - faces[i].x - faces[i].width * 0.5);
+            ellipse(frame, center, cv::Size(faces[i].width * 0.5, faces[i].height * 0.5), 0, 0, 360,
+                    cv::Scalar(255, 0, 255), 4, 8, 0);
+
+        }
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_cvsp_whitechristmas_OpencvCalls_faceTracking(JNIEnv *env, jclass type) {
+
+    // TODO
+
+}
+
+extern "C"
+JNIEXPORT jobjectArray JNICALL
+Java_cvsp_whitechristmas_OpencvCalls_getFaces__J(JNIEnv *env, jclass type, jlong addrRgba) {
+    static jclass rectClass = env->FindClass("org/opencv/core/Rect");
+    if (rectClass == NULL) {
+        __android_log_print(ANDROID_LOG_INFO, "OpenCVCalls", "Error finding rect class"); return nullptr;
+
+    } else {
+        __android_log_print(ANDROID_LOG_INFO, "OpenCVCalls", "rect class created");
+
+    }
+    static jmethodID rectCtorID = env->GetMethodID(rectClass, "<init>", "(IIII)V");
+
+//    if (rectCtorID == 0) {
+//        rectClass = env->FindClass("org/opencv/core/Rect");
+//        rectCtorID = env->GetMethodID(rectClass, "<init>", "(IIII)V");
+//    }
+
+
     cv::Mat& frame = *(cv::Mat*)addrRgba;
     cv::String face_cascade_name = "/storage/emulated/0/Download/haarcascade_frontalface_default.xml";
-    cv::String eyes_cascade_name = "/storage/emulated/0/Download/haarcascade_eye.xml";
     cv::CascadeClassifier face_cascade;
-    cv::CascadeClassifier eyes_cascade;
 
-    if( !face_cascade.load( face_cascade_name ) ){ __android_log_print(ANDROID_LOG_INFO, "OpenCVCalls", "Error loading face cascade"); return; };
-    if( !eyes_cascade.load( eyes_cascade_name ) ){ __android_log_print(ANDROID_LOG_INFO, "OpenCVCalls", "Error loading eye cascade"); return; };
-
+    if( !face_cascade.load( face_cascade_name ) ){ __android_log_print(ANDROID_LOG_INFO, "OpenCVCalls", "Error loading face cascade"); return nullptr; };
 
     std::vector<cv::Rect> faces;
     cv::Mat frame_gray;
@@ -40,26 +105,16 @@ Java_cvsp_whitechristmas_OpencvCalls_faceDetection(JNIEnv *env, jclass type, jlo
     //-- Detect faces
     face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
 
+    jobjectArray detectedFaces = env->NewObjectArray(faces.size(),(jclass)rectClass, 0);
+
     for( size_t i = 0; i < faces.size(); i++ )
     {
-//        cv::Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
         cv::Point center( faces[i].y + faces[i].height*0.5, frame.rows-faces[i].x - faces[i].width*0.5 );
         ellipse( frame, center, cv::Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, cv::Scalar( 255, 0, 255 ), 4, 8, 0 );
 
-        cv::Mat faceROI = frame_gray( faces[i] );
-        std::vector<cv::Rect> eyes;
-
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
-
-        for( size_t j = 0; j < eyes.size(); j++ )
-        {
-//            cv::Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-            cv::Point center( faces[i].y + eyes[j].y + eyes[j].height*0.5, frame.rows-eyes[i].x - eyes[i].width*0.5 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, center, radius, cv::Scalar( 255, 0, 0 ), 4, 8, 0 );
-
-        }
+        cv::Rect currentRect = faces[i];
+        jobject rect = env->NewObject(rectClass, rectCtorID, currentRect.x, currentRect.y, currentRect.width, currentRect.height);
+        env->SetObjectArrayElement(detectedFaces, i, rect);
     }
-
+    return detectedFaces;
 }
--- app/src/main/java/cvsp/whitechristmas/OpencvCalls.java
+++ app/src/main/java/cvsp/whitechristmas/OpencvCalls.java
@@ -1,5 +1,11 @@
 package cvsp.whitechristmas;
 
+import org.opencv.core.Rect;
+
+import java.util.ArrayList;
+
 public class OpencvCalls {
     public native static void faceDetection(long addrRgba);
+    public native static Rect[] getFaces(long addrRgba);
+    public native static void faceTracking();
 }
