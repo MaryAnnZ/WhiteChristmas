7eb0285e24897552bee783f277bf51944db297a8
bettin <betti.schlager@aon.at>
2019-01-06 12:47:17 +0100

    permissions checks

--- app/src/main/java/cvsp/whitechristmas/MainActivity.java
+++ app/src/main/java/cvsp/whitechristmas/MainActivity.java
@@ -1,246 +1,167 @@
 package cvsp.whitechristmas;
-
 import java.util.List;
-
-
-
 import org.opencv.android.BaseLoaderCallback;
-
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-
 import org.opencv.android.LoaderCallbackInterface;
-
 import org.opencv.android.OpenCVLoader;
-
-import org.opencv.core.Core;
-
 import org.opencv.core.CvType;
-
 import org.opencv.core.Mat;
-
-import org.opencv.core.MatOfPoint;
-
-import org.opencv.core.Rect;
-
 import org.opencv.core.Scalar;
-
-import org.opencv.core.Size;
-
 import org.opencv.android.CameraBridgeViewBase;
-
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-
 import org.opencv.imgproc.Imgproc;
-
-
-
+import android.Manifest;
 import android.app.Activity;
-
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
-
 import android.view.MotionEvent;
-
 import android.view.View;
-
 import android.view.Window;
-
 import android.view.WindowManager;
-
 import android.view.View.OnTouchListener;
-
 import android.view.SurfaceView;
-
-
-
 
 public class MainActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
 
-    private static final String  TAG              = "MainActivity";
+    private static final String TAG = "MainActivity";
+    private Mat mRgba;
 
-    private Mat                  mRgba;
-
-//    static{ System.loadLibrary("opencv_java3"); }
-
+    //    static{ System.loadLibrary("opencv_java3"); }
     private CameraBridgeViewBase mOpenCvCameraView;
-
     private OpencvCalls opencvCalls;
 
-    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
-
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
-
         public void onManagerConnected(int status) {
-
             switch (status) {
-
-                case LoaderCallbackInterface.SUCCESS:
-
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
-
                     mOpenCvCameraView.enableView();
-
                     mOpenCvCameraView.setOnTouchListener(MainActivity.this);
-
-                } break;
-
-                default:
-
-                {
-
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-
-                } break;
-
+                }
+                break;
             }
-
         }
-
     };
 
 
-
     public MainActivity() {
-
         Log.i(TAG, "Instantiated new " + this.getClass());
         System.loadLibrary("native-lib");
     }
 
 
-
-    /** Called when the activity is first created. */
-
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-
     public void onCreate(Bundle savedInstanceState) {
-
         Log.i(TAG, "called onCreate");
-
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        setContentView(R.layout.activity_main);
 
 
-        setContentView(R.layout .activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    0);
 
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
 
-        //TODO: update name
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
-
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-
-        mOpenCvCameraView.setCvCameraViewListener(this);
-
-    }
-
-
-
-    @Override
-
-    public void onPause()
-
-    {
-
-        super.onPause();
-
-        if (mOpenCvCameraView != null)
-
-            mOpenCvCameraView.disableView();
-
-    }
-
-
-
-    @Override
-
-    public void onResume()
-
-    {
-
-        super.onResume();
-
-        if (!OpenCVLoader.initDebug()) {
-
-            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-
-        } else {
-
-            Log.d(TAG, "OpenCV library found inside package. Using it!");
-
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    2);
 
         }
 
+        startCameraView();
     }
 
+    private void startCameraView() {
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+            if (!OpenCVLoader.initDebug()) {
+                Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+                OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+            } else {
+                Log.d(TAG, "OpenCV library found inside package. Using it!");
+                mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            }
+    }
 
 
     public void onDestroy() {
-
         super.onDestroy();
-
         if (mOpenCvCameraView != null)
-
             mOpenCvCameraView.disableView();
-
     }
-
 
 
     public void onCameraViewStarted(int width, int height) {
-
         mRgba = new Mat(height, width, CvType.CV_8UC4);
-
     }
-
 
 
     public void onCameraViewStopped() {
-
         mRgba.release();
-
     }
-
 
 
     public boolean onTouch(View v, MotionEvent event) {
 
-
         return false; // don't need subsequent touch events
-
     }
-
 
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-
-        mRgba = inputFrame.rgba();
-        opencvCalls.faceDetection(mRgba.getNativeObjAddr());
-        return mRgba;
-
+            mRgba = inputFrame.rgba();
+            opencvCalls.faceDetection(mRgba.getNativeObjAddr());
+            return mRgba;
     }
-
 
 
     private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
-
         Mat pointMatRgba = new Mat();
-
         Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
-
         Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
-
-
-
         return new Scalar(pointMatRgba.get(0, 0));
-
     }
-
 }
